744fd5343cc65e244904cedb0201ad3728280f50
Maciej Pryc <pryc.maciej@gmail.com>
2014-12-14 17:28:38 +0100

    Some reorganizations and cleaning in Model class

--- src/com/hardride/Model.java
+++ src/com/hardride/Model.java
@@ -27,51 +27,16 @@ public class Model {
 	protected int mVBO[] = new int[1];
 	protected int mIBO[] = new int[1];
 	
-	protected int FLOATS_PER_POSITION;
-	protected int FLOATS_PER_NORMAL;
-	protected float mVertices[];
-	protected float mNormals[];
-	protected short mIndices[];
+	protected int FLOATS_PER_POSITION = 3;
+	protected int FLOATS_PER_NORMAL = 3;
+	
+	protected float mVerticesRawData[];
+	protected short mIndicesRawData[];
 	protected int mIndicesAmount;
 	
     public Model(String modelName, Context context) {
-    	loadModel(modelName, context);
-    	
-        ByteBuffer dlb = ByteBuffer.allocateDirect(mIndices.length * Actor.BYTES_PER_SHORT);
-        dlb.order(ByteOrder.nativeOrder());
-        ShortBuffer indexBuffer = dlb.asShortBuffer();
-        indexBuffer.put(mIndices);
-        indexBuffer.position(0);
-        
-        float [] verticesData = new float[mVertices.length * 2];
-        for (int i = 0; i < mVertices.length; i += 3) {
-        	verticesData[2*i] = mVertices[i];
-        	verticesData[2*i+1] = mVertices[i+1];
-        	verticesData[2*i+2] = mVertices[i+2];
-        	verticesData[2*i+3] = mNormals[i];
-        	verticesData[2*i+4] = mNormals[i+1];
-        	verticesData[2*i+5] = mNormals[i+2];
-        }
-        
-        ByteBuffer vbb = ByteBuffer.allocateDirect(verticesData.length * Actor.BYTES_PER_FLOAT);
-        vbb.order(ByteOrder.nativeOrder());
-        FloatBuffer newVertexBuffer = vbb.asFloatBuffer();
-        newVertexBuffer.put(verticesData);
-        newVertexBuffer.position(0);
-        
-        GLES20.glGenBuffers(1, mVBO, 0);
-        GLES20.glGenBuffers(1, mIBO, 0);
-               
-        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, mVBO[0]);
-        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, newVertexBuffer.capacity() * Actor.BYTES_PER_FLOAT,
-        		newVertexBuffer, GLES20.GL_STATIC_DRAW);       
-
-        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, mIBO[0]);
-        GLES20.glBufferData(GLES20.GL_ELEMENT_ARRAY_BUFFER, indexBuffer.capacity() * Actor.BYTES_PER_SHORT, 
-        		indexBuffer, GLES20.GL_STATIC_DRAW);
-        
-        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
-        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
+    	loadModelDataFromFile(modelName, context);
+    	sendModelDataToGPUBuffers();
     }
 
 	public void draw(BaseObjectShaderSet shader) {
@@ -88,7 +53,35 @@ public class Model {
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, mIndicesAmount, GLES20.GL_UNSIGNED_SHORT, 0);
     }
     
-	protected void loadModel(String modelName, Context context) {
+	protected void sendModelDataToGPUBuffers() {
+        ByteBuffer vbb = ByteBuffer.allocateDirect(mVerticesRawData.length * Actor.BYTES_PER_FLOAT);
+        vbb.order(ByteOrder.nativeOrder());
+        FloatBuffer newVertexBuffer = vbb.asFloatBuffer();
+        newVertexBuffer.put(mVerticesRawData);
+        newVertexBuffer.position(0);
+        
+        ByteBuffer ibb = ByteBuffer.allocateDirect(mIndicesRawData.length * Actor.BYTES_PER_SHORT);
+        ibb.order(ByteOrder.nativeOrder());
+        ShortBuffer indexBuffer = ibb.asShortBuffer();
+        indexBuffer.put(mIndicesRawData);
+        indexBuffer.position(0);
+        
+        GLES20.glGenBuffers(1, mVBO, 0);
+        GLES20.glGenBuffers(1, mIBO, 0);
+               
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, mVBO[0]);
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, newVertexBuffer.capacity() * Actor.BYTES_PER_FLOAT,
+        		newVertexBuffer, GLES20.GL_STATIC_DRAW);       
+
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, mIBO[0]);
+        GLES20.glBufferData(GLES20.GL_ELEMENT_ARRAY_BUFFER, indexBuffer.capacity() * Actor.BYTES_PER_SHORT, 
+        		indexBuffer, GLES20.GL_STATIC_DRAW);
+        
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);		
+	}
+	
+	protected void loadModelDataFromFile(String modelName, Context context) {
 		List<String> strLines = new ArrayList<String>();
 		
 		try {
@@ -105,7 +98,6 @@ public class Model {
 		
 		boolean headerRead = false;
 		int verticesAmount;
-		int indicesAmount;
 		
 		int vIdx = 0, nIdx = 0, iIdx = 0;
 		
@@ -116,38 +108,33 @@ public class Model {
 				if (!headerRead) {
 					String[] tokens = line.split("[ ]+");
 					verticesAmount = Integer.parseInt(tokens[0]);
-					FLOATS_PER_POSITION = Integer.parseInt(tokens[1]);
-					FLOATS_PER_NORMAL = FLOATS_PER_POSITION;
-					indicesAmount = Integer.parseInt(tokens[2]);
+					mIndicesAmount = Integer.parseInt(tokens[1]);
 					
-					mVertices = new float[FLOATS_PER_POSITION * verticesAmount];
-					mNormals = new float[FLOATS_PER_NORMAL * verticesAmount];
-					mIndices = new short[indicesAmount];
+					mVerticesRawData = new float[(FLOATS_PER_POSITION + FLOATS_PER_NORMAL) * verticesAmount];
+					mIndicesRawData = new short[mIndicesAmount];
 					headerRead = true;
 				}
 				else if (line.startsWith("v ")) {
 					String[] tokens = line.split("[ ]+");
-					mVertices[vIdx] = Float.parseFloat(tokens[1]);
-					mVertices[vIdx + 1] = Float.parseFloat(tokens[2]);
-					mVertices[vIdx + 2] = Float.parseFloat(tokens[3]);
+					mVerticesRawData[2 * vIdx] = Float.parseFloat(tokens[1]);
+					mVerticesRawData[2 * vIdx + 1] = Float.parseFloat(tokens[2]);
+					mVerticesRawData[2 * vIdx + 2] = Float.parseFloat(tokens[3]);
 					vIdx += 3;
 				}
 				else if (line.startsWith("vn ")) {
 					String[] tokens = line.split("[ ]+");
-					mNormals[nIdx] = Float.parseFloat(tokens[1]);
-					mNormals[nIdx + 1] = Float.parseFloat(tokens[2]);
-					mNormals[nIdx + 2] = Float.parseFloat(tokens[3]);
+					mVerticesRawData[2 * nIdx + 3] = Float.parseFloat(tokens[1]);
+					mVerticesRawData[2 * nIdx + 4] = Float.parseFloat(tokens[2]);
+					mVerticesRawData[2 * nIdx + 5] = Float.parseFloat(tokens[3]);
 					nIdx += 3;
 				}
 				else if (line.startsWith("f ")) {
 					String[] tokens = line.split("[ ]+");
 					for (int j = 1; j < tokens.length; j++) {
-						mIndices[iIdx++] = Short.parseShort(tokens[j]);
+						mIndicesRawData[iIdx++] = Short.parseShort(tokens[j]);
 					}
 				}			
 			}
 		}
-		
-		mIndicesAmount = mIndices.length;
 	}
 }
