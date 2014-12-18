a5c164ab8ce2dc8273374b702e2504499b45c12a
Maciej Pryc <pryc.maciej@gmail.com>
2014-12-19 00:38:22 +0100

    Time-based movement for vehicle

--- src/com/hardride/HardRideLogic.java
+++ src/com/hardride/HardRideLogic.java
@@ -7,6 +7,7 @@
 
 package com.hardride;
 
+import android.os.SystemClock;
 import com.hardride.actors.VehicleActor;
 
 public class HardRideLogic {
@@ -19,34 +20,40 @@ public class HardRideLogic {
 	private int mInput[] = new int[InputType.size];
 	
 	private VehicleActor mVehicle;
-	private float mMoveSpeed[] = new float[]{0.0f, 1.0f}; 
-	private final float mBaseSpeed = 5.0f;
+	private float mMoveDir[] = new float[]{0.0f, 1.0f}; 
+	private final float mBaseSpeed = 20.0f;
 	private float mAngle = 0.0f;
+	
+	private float mLastUpdateTime;
 	
 	public HardRideLogic() {
 		mInput[InputType.NONE.ordinal()] = 2;
+		mLastUpdateTime = SystemClock.uptimeMillis();
 	}
 	
 	public void update() {
-		float dT = 0.1f;
+		float currTime = SystemClock.uptimeMillis();
+		float dT = (currTime - mLastUpdateTime) / 1000.0f;
 		
-		mVehicle.setX(mVehicle.getX() + mMoveSpeed[0] * dT * mBaseSpeed);
-		mVehicle.setZ(mVehicle.getZ() + mMoveSpeed[1] * dT * mBaseSpeed);
+		mVehicle.setX(mVehicle.getX() + mMoveDir[0] * dT * mBaseSpeed);
+		mVehicle.setZ(mVehicle.getZ() + mMoveDir[1] * dT * mBaseSpeed);
 		
-		mRenderer.updateViewMatrix(mVehicle.getX(), mVehicle.getZ(), mMoveSpeed[0], mMoveSpeed[1]);
+		mRenderer.updateViewMatrix(mVehicle.getX(), mVehicle.getZ(), mMoveDir[0], mMoveDir[1]);
 		
 		if (mInput[InputType.LEFT.ordinal()] > 0) {
 			mAngle += 0.05f;
 			mVehicle.setPitch((float) Math.toDegrees(mAngle));
-			mMoveSpeed[0] = (float) Math.sin(mAngle);
-			mMoveSpeed[1] = (float) Math.cos(mAngle);
+			mMoveDir[0] = (float) Math.sin(mAngle);
+			mMoveDir[1] = (float) Math.cos(mAngle);
 		}
 		if (mInput[InputType.RIGHT.ordinal()] > 0) {
 			mAngle -= 0.05f;
 			mVehicle.setPitch((float) Math.toDegrees(mAngle));
-			mMoveSpeed[0] = (float) Math.sin(mAngle);
-			mMoveSpeed[1] = (float) Math.cos(mAngle);
+			mMoveDir[0] = (float) Math.sin(mAngle);
+			mMoveDir[1] = (float) Math.cos(mAngle);
 		}
+		
+		mLastUpdateTime = currTime;
 	}
 	
 	public void setRenderer(HardRideRenderer renderer) {
